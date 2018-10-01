a235509cc74d4188e7f5812de604d61f449d4221
Dmitry V. Alekseev <mail@alexeevdv.ru>
2018-10-01 12:57:10 +0300

    there is no ::class in 5.4 :(

--- tests/SegfaultExceptionTest.php
+++ tests/SegfaultExceptionTest.php
@@ -7,7 +7,7 @@ class SegfaultExceptionTest extends \Codeception\Test\Unit
     public function testToString()
     {
         $exception = new SegfaultException;
-        $this->expectException(SegfaultException::class);
+        $this->expectException('alexeevdv\SegfaultException');
         $exception->__toString();
     }
 
