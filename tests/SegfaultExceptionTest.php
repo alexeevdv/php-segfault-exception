bb9487b9e741c4cc2ab75742386131e85aac3540
Dmitry V. Alekseev <mail@alexeevdv.ru>
2018-10-01 12:40:52 +0300

    lets have a real segfault and test over PHP7.3

--- tests/SegfaultExceptionTest.php
+++ tests/SegfaultExceptionTest.php
@@ -1,13 +1,10 @@
 <?php
 
-use alexeevdv\SegfaultException;
-
 class SegfaultExceptionTest extends \Codeception\Test\Unit
 {
     public function testToString()
     {
-        $exception = new SegfaultException;
-        $this->expectException(SegfaultException::class);
-        $exception->__toString();
+        $result = exec('php tests/_support/shell.php', $output);
+        $this->stringContains($result, 'Segmenation fault');
     }
 }
---
+++ tests/_support/shell.php
@@ -0,0 +1,5 @@
+<?php
+
+include __DIR__ . '/../../src/SegfaultException.php';
+
+throw new \alexeevdv\SegfaultException;
